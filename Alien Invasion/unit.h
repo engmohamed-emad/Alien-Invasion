344102ced258fa03a106f429d6bb3ef2549db484
omar <omarayman740@yahoo.com>
2024-04-05 17:49:11 +0200

    edit 3

--- Alien Invasion/unit.h
+++ Alien Invasion/unit.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef unit_h
+#define unit_h
+#include <string>
+using namespace std;
 //Abstract class for all army units
 //mmooomayman mohamed 24
 class unit
@@ -6,8 +9,9 @@ class unit
 private:
 	int ID;
 	int health;
-
+	string s1;
 public:
 
 };
+#endif
 
