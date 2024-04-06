856edcae7373870f01d8b2a71296e3d977fdbcf3
HossamMo123 <mah24719782471978@gmail.com>
2024-04-06 02:53:21 +0200

    unit Abstract class
    if you have any additional function for the class inform me .

--- Alien Invasion/unit.cpp
+++
@@ -1,2 +0,0 @@
-#include "unit.h"
-//mohamed ayman mohamed  
--- Alien Invasion/unit.h
+++ Alien Invasion/unit.h
@@ -1,16 +1,78 @@
 #ifndef unit_h
 #define unit_h
+#pragma once
 #include <string>
 using namespace std;
 //Abstract class for all army units
-//mmooomayman mohamed 24
 class unit
 {
 private:
 	int ID;
-	int health;
-	string s1;
+	float health;
+	float currhealth;
+	int Jtime;
+	float Apower;
+	int Acapacity;
+	string type;
 public:
+	void setID(int id)
+	{
+		this->ID = id;
+	}
+	int getID()
+	{
+		return ID;
+	}
+	void sethealth(float h)
+	{
+		this->health = h;
+	}
+	int getcurrhealth()
+	{
+		return currhealth;
+	}
+	void setJtime(int t)
+	{
+		Jtime = t;
+	}
+	int getJtime()
+	{
+		return Jtime;
+	}
+	void setApower(float p)
+	{
+		Apower = p;
+	}
+	int getApower()
+	{
+		return Apower;
+	}
+	void setAcapacity(int c)
+	{
+		Acapacity = c;
+	}
+	int getAcapacity()
+	{
+		return Acapacity;
+	}
+
+	virtual void dec_health(float damege) = 0;
+	virtual void inc_health(float heal) {}
+	virtual bool is_dead()
+	{
+		if (currhealth <= 0)
+			return true;
+		return false;
+	}
+	virtual bool need_help()
+	{
+		if ((currhealth / health) < 0.2)
+			return true;
+		return false;
+	}
+	virtual int attack(int enhealth) = 0;
+
+
 
 };
 #endif
