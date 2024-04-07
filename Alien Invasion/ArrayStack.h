b467453f519c49e18dc7b5ace408bd511656c84b
engmohamed-emad <149832636+engmohamed-emad@users.noreply.github.com>
2024-04-07 17:30:07 +0200

    edited added files

--- Alien Invasion/ArrayStack.h
+++ Alien Invasion/ArrayStack.h
@@ -1,29 +1,31 @@
-#ifndef ArrayStack_h
-#define ArrayStack_h
+#pragma once
+//	This is an updated version of code originally
+//  created by Frank M. Carrano and Timothy M. Henry.
+//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
 
+/** ADT stack: Array-based implementation.
+ @file ArrayStack.h */
 
+#ifndef ARRAY_STACK_
+#define ARRAY_STACK_
+
+#include "StackADT.h"
+
+ //Unless spesificed by the stack user, the default size is 100
 template<typename T>
-class ArrayStack  
+class ArrayStack : public StackADT<T>
 {
+	enum { MAX_SIZE = 100 };
 private:
-	T* items;		// Array of stack items
-	int      top;                   // Index to top of stack
-	const int STACK_SIZE;
+	T items[MAX_SIZE];		// Array of stack items
+	int top;                   // Index to top of stack
 
 public:
 
-	ArrayStack(int MaxSize) : STACK_SIZE(MaxSize)
+	ArrayStack()
 	{
-		items = new T[STACK_SIZE];
 		top = -1;
 	}  // end default constructor
-
-	//Function getCapacity() returns the stack max size
-	//added for array implementaion only // (14)
-	int getCapacity()
-	{
-		return STACK_SIZE;
-	}
 
 	bool isEmpty() const
 	{
@@ -32,7 +34,7 @@ public:
 
 	bool push(const T& newEntry)
 	{
-		if (top == STACK_SIZE - 1) return false;	//Stack is FULL
+		if (top == MAX_SIZE - 1) return false;	//Stack is FULL
 
 		top++;
 		items[top] = newEntry;
@@ -56,24 +58,6 @@ public:
 		return true;
 	}  // end peek
 
-	//Destructor
-	~ArrayStack()
-	{
-		delete[]items;
-	}
-
-	//Copy constructor
-	ArrayStack(const ArrayStack<T>& S) :STACK_SIZE(S.STACK_SIZE)
-	{
-		items = new T[STACK_SIZE];
-		for (int i = 0; i <= S.top; i++)
-			items[i] = S.items[i];
-		top = S.top;
-	}
-
 }; // end ArrayStack
-
-
-
 
 #endif
--- Alien Invasion/Node.h
+++ Alien Invasion/Node.h
@@ -1,5 +1,7 @@
-#ifndef Node_h
-#define Node_h
+#pragma once
+
+#ifndef _NODE
+#define _NODE
 template < typename T>
 class Node
 {
@@ -7,45 +9,55 @@ private:
 	T item; // A data item
 	Node<T>* next; // Pointer to next node
 public:
-
-	Node()
-	{
-		next = nullptr;
-	}
-
-	Node(const T& r_Item)
-	{
-		item = r_Item;
-		next = nullptr;
-	}
-
-	Node(const T& r_Item, Node<T>* nextNodePtr)
-	{
-		item = r_Item;
-		next = nextNodePtr;
-	}
-	void setItem(const T& r_Item)
-	{
-		item = r_Item;
-	}
-
-	void setNext(Node<T>* nextNodePtr)
-	{
-		next = nextNodePtr;
-	}
-
-	T getItem() const
-	{
-		return item;
-	}
-
-	Node<T>* getNext() const
-	{
-		return next;
-	}
-
-};
-
-
+	Node();
+	Node(const T& r_Item);
+	Node(const T& r_Item, Node<T>* nextNodePtr);
+	void setItem(const T& r_Item);
+	void setNext(Node<T>* nextNodePtr);
+	T getItem() const;
+	Node<T>* getNext() const;
+}; // end Node
 #endif
 
+template < typename T>
+Node<T>::Node()
+{
+	next = nullptr;
+}
+
+template < typename T>
+Node<T>::Node(const T& r_Item)
+{
+	item = r_Item;
+	next = nullptr;
+}
+
+template < typename T>
+Node<T>::Node(const T& r_Item, Node<T>* nextNodePtr)
+{
+	item = r_Item;
+	next = nextNodePtr;
+}
+template < typename T>
+void Node<T>::setItem(const T& r_Item)
+{
+	item = r_Item;
+}
+
+template < typename T>
+void Node<T>::setNext(Node<T>* nextNodePtr)
+{
+	next = nextNodePtr;
+}
+
+template < typename T>
+T Node<T>::getItem() const
+{
+	return item;
+}
+
+template < typename T>
+Node<T>* Node<T>::getNext() const
+{
+	return next;
+}
